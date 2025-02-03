90cba40c4b63498fdffaeaf489c7758ce2ebfa6e
s0vadav1d <david_sova@ukr.net>
2025-02-03 13:14:40 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from './components/Button';
 
 function App() {
+  const button1Foo = (subscriber: string, age: number, address: string) => {
+    console.log(subscriber);
+  }
+
+  const button2Foo = (subscriber: string) => {
+    console.log(subscriber);
+  }
+  const button3Foo = () => {
+    console.log("I'm button");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button
+        name={"First Button"}
+        callBack={() => button1Foo("Travor", 21, "Kyiv")}
+      />
+      <Button name={"Second Button"} callBack={() => button2Foo("Manson")} />
+      <Button name={"Second Button"} callBack={() => button3Foo()} />
     </div>
   );
 }
