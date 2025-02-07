147034368b8a7e4b9cb07379ad7fa292582df1b8
s0vadav1d <david_sova@ukr.net>
2025-02-07 13:01:56 +0100

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button } from './components/Button';
 
 function App() {
-  const button1Foo = (subscriber: string, age: number, address: string) => {
-    console.log(subscriber);
-  }
-
-  const button2Foo = (subscriber: string) => {
-    console.log(subscriber);
-  }
-  const button3Foo = () => {
-    console.log("I'm button");
-  };
+  const [money, setMoney] = useState([
+    { banknote: "dollar", nominal: 100, number: "a123456789" },
+    { banknote: "dollar", nominal: 50, number: "b123456789" },
+    { banknote: "ruble", nominal: 100, number: "c123456789" },
+    { banknote: "dollar", nominal: 100, number: "d123456789" },
+    { banknote: "dollar", nominal: 50, number: "e123456789" },
+    { banknote: "ruble", nominal: 100, number: "f123456789" },
+    { banknote: "dollar", nominal: 50, number: "j123456789" },
+    { banknote: "ruble", nominal: 50, number: "h123456789" },
+  ]);
   return (
-    <div className="App">
-      <Button
-        name={"First Button"}
-        callBack={() => button1Foo("Travor", 21, "Kyiv")}
-      />
-      <Button name={"Second Button"} callBack={() => button2Foo("Manson")} />
-      <Button name={"Second Button"} callBack={() => button3Foo()} />
-    </div>
+    <ul>
+      {money.map((objFromMoneyArr) => {
+        return (
+          <li>
+            <span>{objFromMoneyArr.banknote}</span>
+            <span>{objFromMoneyArr.nominal}</span>
+            <span>{objFromMoneyArr.number}</span>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
